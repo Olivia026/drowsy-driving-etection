45cc18a64263179508c4477265f71a4c5f82e4b7
Olivia <153999057+Olivia026@users.noreply.github.com>
2025-04-18 15:04:32 +0800

    packagejoson-changed

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,33 @@
 module.exports = {
-  lintOnSave: false,
-  devServer: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://xxxxx.com:9093',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '',
+    lintOnSave: false,
+    devServer: {
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://xxxxx.com:9093',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '',
+                }
+            }
         }
-      }
+        publicPath: process.env.NODE_ENV === 'production' ?
+            '/drowsy-driving-etection/' // 替换为仓库名
+            :
+            '/'
+    },
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    require("postcss-px-to-viewport")({
+                        "viewportWidth": 1920,
+                        "unitPrecision": 5,
+                        "minPixelValue": 1,
+                        "exclude": [/components\/demo/]
+                    })
+                ]
+            }
+        }
     }
-  },
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: [
-          require("postcss-px-to-viewport")({
-            "viewportWidth": 1920,
-            "unitPrecision": 5,
-            "minPixelValue": 1,
-            "exclude": [/components\/demo/]
-          })
-        ]
-      }
-    }
-  }
 }
